7009d561e740fcbfdfaaedf442ea2b4bdc514ec1
Kirguir <a.shutiy@gmail.com>
2019-09-25 12:26:29 +0300

    Fix catch error

--- demo/js/index.js
+++ demo/js/index.js
@@ -1,19 +1,22 @@
+import {MyError} from "../../src/error";
+
 const rust = import('../../pkg');
 rust
   .then(rust => {
     let foo = new rust.Foo();
 
-    foo.run()
-      .catch(function (err) {
-      if (err instanceof Error) {
-        console.log("Standart error");
-      } else if (err instanceof MyError) {
+    try {
+      foo.run()
+    } catch (err) {
+      if (err instanceof MyError) {
         console.log("name:" + err.name());
         console.log("description:" + err.message());
+      } else if (err instanceof Error) {
+        console.log("Standart error");
       } else {
         console.log("Unknown error");
       }
-    });
+    }
     console.log("End");
     foo.dispose();
   });
